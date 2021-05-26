1de1cf5bd5295193130050bb691b9a83de855538
Minoxs <gufrgs2018@gmail.com>
2021-05-25 22:17:16 -0300

    computing huffman tree

--- src/Text/HuffmanEncoding.c
+++ src/Text/HuffmanEncoding.c
@@ -5,7 +5,24 @@
 
 #include "HuffmanEncoding.h"
 
-// TODO 'Create Encoding Tree' (CountedLetterTable) -> (EncodingTree)
+/**
+ * Creates the encoding tree given the counted letters in order
+ * @param letters
+ * @return
+ */
+HuffmanTree* createEncodingTree(CountedLetters* letters) {
+    int size = letters->arrSize;
+    HuffmanList* list = countedLettersToHuffmanList(letters);
+
+    while (size > 1) {
+        HuffmanTree* elem1 = popFromStart(&list);
+        HuffmanTree* elem2 = popFromStart(&list);
+        insertHList(&list, appendTwoElements(elem1, elem2));
+        size -= 1;
+    }
+
+    return popFromStart(&list);
+}
 
 // TODO 'Encode Text' (Text, EncodingTree) -> (EncodedText)
 
