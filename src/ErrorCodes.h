dca2ad3570fcf441ac52ed007f6e90d22297e2bf
Minoxs <gufrgs2018@gmail.com>
2021-06-04 01:42:47 -0300

    global list of error codes

---
+++ src/ErrorCodes.h
@@ -0,0 +1,11 @@
+//
+// Author: Mino
+// Github: https://github.com/minoxs
+//
+#ifndef HUFFMAN_ERRORCODES_H
+#define HUFFMAN_ERRORCODES_H
+
+#define NULL_TABLE_ERROR 1
+#define NULL_TREE_ERROR  2
+
+#endif //HUFFMAN_ERRORCODES_H
