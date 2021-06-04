70f4a0c12f2d4236627bc786d0d1c7ab89cfdcef
Minoxs <gufrgs2018@gmail.com>
2021-06-04 01:43:54 -0300

    functions that run the full encode/decode process given the file inputs/outputs

--- src/FileHandler/FileHandler.c
+++ src/FileHandler/FileHandler.c
@@ -4,6 +4,9 @@
 //
 
 #include "FileHandler.h"
+#include "../Text/TextCounter.h"
+#include "../DataStructs/Huffman/HuffmanTree.h"
+#include "../Text/HuffmanEncoding.h"
 
 FILE* writeTextfile(char* filename) {
     return fopen(filename, "w");
@@ -19,4 +22,39 @@ FILE* writeBinaryFile(char* filename) {
 
 FILE* readBinaryFile(char* filename) {
     return fopen(filename, "rb");
+}
+
+/**
+ * Takes in all required files and does everything needed to encode the text
+ * @param text File in "r" mode
+ * @param encodedTextOutput File in "wb" mode
+ * @param encodedTreeOutput File in "wb" mode
+ * @return
+ */
+int RunEncodeText(FILE* text, FILE* encodedTextOutput, FILE* encodedTreeOutput) {
+    CountedLetters* table = parseText(text);
+    if (table == NULL)
+        return NULL_TABLE_ERROR;
+
+    HuffmanTree* tree = createEncodingTree(table);
+    saveTreeToFile(tree, encodedTreeOutput);
+    encodeText(text, tree, encodedTextOutput);
+
+    return 0;
+}
+
+/**
+ * Takes in all required files and does everything needed to decode the text
+ * @param encodedText File in "rb"
+ * @param encodedTree File in "rb"
+ * @param output File in "w" or NULL (will print to console)
+ */
+int RunDecodeText(FILE* encodedText, FILE* encodedTree, FILE* output) {
+    HuffmanTree* tree = loadTreeFromFile(encodedTree);
+    if (tree == NULL)
+        return NULL_TREE_ERROR;
+
+    decodeText(encodedText, tree, output);
+
+    return 0;
 }
