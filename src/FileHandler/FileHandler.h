4d2346975b2c9c44cb34271484843c87011a6687
Mino <dev@mino.one>
2021-06-04 01:43:54 -0300

    functions that run the full encode/decode process given the file inputs/outputs

--- src/FileHandler/FileHandler.h
+++ src/FileHandler/FileHandler.h
@@ -7,10 +7,14 @@
 #define FILE_HANDLER_H
 
 #include <stdio.h>
+#include "../ErrorCodes.h"
 
 FILE* writeTextfile(char* filename);
 FILE* readTextfile(char* filename);
 FILE* writeBinaryFile(char* filename);
 FILE* readBinaryFile(char* filename);
+
+int RunEncodeText(FILE* text, FILE* encodedTextOutput, FILE* encodedTreeOutput);
+int RunDecodeText(FILE* encodedText, FILE* encodedTree, FILE* output);
 
 #endif //FILE_HANDLER_H
