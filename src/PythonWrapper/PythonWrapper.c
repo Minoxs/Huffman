da23e24b949eeda27f9f4716cba886e286380019
Mino <gufrgs2018@gmail.com>
2022-02-13 13:58:27 -0300

    Python wrapper added!

--- src/PythonWrapper/PythonWrapper.c
+++ src/PythonWrapper/PythonWrapper.c
@@ -3,42 +3,90 @@
 // Github: https://github.com/minoxs
 //
 
-// THIS WAS STOLEN FROM RealPython's WEBSITE -- THIS IS JUST AN EXAMPLE OF HOW CPython wrapper works
-// Slightly modified by me so it is compiled with CMake rather than distutils
-// I will use this example to create a CPython wrapper for this library im making :)
-
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <stdio.h>
+#include "../FileHandler/FileHandler.h"
 #define DLLEXPORT __declspec(dllexport)
 
-static PyObject *method_fputs(PyObject *self, PyObject *args) {
-    char *str, *filename = NULL;
+static PyObject* EncodeText(PyObject* self, PyObject* args) {
+    char *textInput = NULL, *encodedTextOutput = NULL, *encodedTreeOutput = NULL;
 
-    int bytes_copied = -1;
-    /* Parse arguments */
-    if(!PyArg_ParseTuple(args, "ss", &str, &filename)) {
-        return NULL;
+    if (!PyArg_ParseTuple(args, "sss", &textInput, &encodedTextOutput, &encodedTreeOutput)) {
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_PARSE_ARGS), PyLong_FromLong(0));
     }
 
-    FILE *fp = fopen(filename, "w");
-    bytes_copied = fputs(str, fp);
-    fclose(fp);
-    return PyLong_FromLong(bytes_copied);
+    FILE* inputFile = readTextfile(textInput);
+    if (inputFile == NULL) {
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_READ_FILE_ERROR), PyLong_FromLong(0));
+    }
+    FILE* textOutputFile = writeBinaryFile(encodedTextOutput);
+    if (textOutputFile == NULL) {
+        fclose(inputFile);
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_WRITE_FILE_ERROR), PyLong_FromLong(1));
+    }
+    FILE* treeOutputFile = writeBinaryFile(encodedTreeOutput);
+    if (treeOutputFile == NULL) {
+        fclose(inputFile);
+        fclose(textOutputFile);
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_WRITE_FILE_ERROR), PyLong_FromLong(2));
+    }
+
+    int returnCode = RunEncodeText(inputFile, textOutputFile, treeOutputFile);
+
+    fclose(inputFile);
+    fclose(textOutputFile);
+    fclose(treeOutputFile);
+
+    return PyLong_FromLong(returnCode);
 }
 
-static PyMethodDef FputsMethods[] = {
-        {"fputs", method_fputs, METH_VARARGS, "Python interface for fputs C library function"},
+static PyObject* DecodeText(PyObject* self, PyObject* args) {
+    char *output = NULL, *encodedTextInput = NULL, *encodedTreeInput = NULL;
+
+    if (!PyArg_ParseTuple(args, "sss", &encodedTextInput, &encodedTreeInput, &output)) {
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_PARSE_ARGS), PyLong_FromLong(0));
+    }
+
+    FILE* encodedTextFile = readBinaryFile(encodedTextInput);
+    if (encodedTextFile == NULL) {
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_READ_FILE_ERROR), PyLong_FromLong(0));
+    }
+    FILE* encodedTreeFile = readBinaryFile(encodedTreeInput);
+    if (encodedTreeFile == NULL) {
+        fclose(encodedTextFile);
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_READ_FILE_ERROR), PyLong_FromLong(1));
+    }
+    FILE* outputFile = writeTextfile(output);
+    if (outputFile == NULL) {
+        fclose(encodedTextFile);
+        fclose(encodedTreeFile);
+        return PyTuple_Pack(2, PyLong_FromLong(FAILED_TO_WRITE_FILE_ERROR), PyLong_FromLong(2));
+    }
+
+    int returnCode = RunDecodeText(encodedTextFile, encodedTreeFile, outputFile);
+
+    fclose(encodedTextFile);
+    fclose(encodedTreeFile);
+    fclose(outputFile);
+
+    return PyLong_FromLong(returnCode);
+}
+
+static PyMethodDef HuffmanMethods[] = {
+        {"EncodeText", EncodeText, METH_VARARGS, "Encode text file"},
+        {"DecodeText", DecodeText, METH_VARARGS, "Decode text file"},
         {NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef fputsmodule = {
+static struct PyModuleDef HuffmanModule = {
         PyModuleDef_HEAD_INIT,
-        "fputs_teste",
-        "Python interface for the fputs C library function",
+        "HuffmanEncoding",
+        "Wrapper for the Huffman text encoding made in C",
         -1,
-        FputsMethods
+        HuffmanMethods
 };
 
-DLLEXPORT PyMODINIT_FUNC PyInit_fputs_teste(void) {
-    return PyModule_Create(&fputsmodule);
+DLLEXPORT PyMODINIT_FUNC PyInit_huffman_c(void) {
+    return PyModule_Create(&HuffmanModule);
 }
